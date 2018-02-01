AllenInstitute/ZeroMQ-XOP

commit 11b20a76bea066eb14c5b96cd15e2c1f4243f16b

Properly construct JSON object in success case as well
This is much cleaner.

src/CallFunctionOperation.cpp
@@ -31,23 +31,28 @@ std::string GetTypeStringForIgorType(int igorType)
   }
 }
 
-std::string ExtractReturnStringFromUnion(IgorTypeUnion *ret, int returnType)
+json ExtractReturnValueFromUnion(IgorTypeUnion *ret, int returnType)
 {
-  std::string result;
-  bool needsQuotes = false;
-
   switch(returnType)
   {
   case NT_FP64:
-    result      = To_stringHighRes(ret->variable);
-    needsQuotes = !std::isfinite(ret->variable);
+    if(isfinite(ret->variable))
+    {
+      return json::parse(To_stringHighRes(ret->variable));
+    }
+    else
+    {
+      return To_stringHighRes(ret->variable);
+    }
     break;
   case HSTRING_TYPE:
-    result = GetStringFromHandle(ret->stringHandle);
+  {
+    auto result = GetStringFromHandle(ret->stringHandle);
     WMDisposeHandle(ret->stringHandle);
     ret->stringHandle = nullptr;
-    needsQuotes       = true;
-    break;
+    return result;
+  }
+  break;
   case WAVE_TYPE:
     if(ret->waveHandle)
     {
@@ -57,23 +62,15 @@ std::string ExtractReturnStringFromUnion(IgorTypeUnion *ret, int returnType)
         throw RequestInterfaceException(REQ_UNSUPPORTED_FUNC_RET);
       }
     }
-    result = SerializeWave(ret->waveHandle);
+    return SerializeWave(ret->waveHandle);
     break;
   case DATAFOLDER_TYPE:
-    result      = SerializeDataFolder(ret->dataFolderHandle);
-    needsQuotes = true;
+    return SerializeDataFolder(ret->dataFolderHandle);
     break;
   default:
     ASSERT(0);
     break;
   }
-
-  if(needsQuotes)
-  {
-    return "\"" + result + "\"";
-  }
-
-  return result;
 }
 
 } // anonymous namespace
@@ -281,21 +278,11 @@ json CallFunctionOperation::Call() const
     throw RequestInterfaceException(REQ_FUNCTION_ABORTED);
   }
 
-  auto retJSONTemplate = R"(
-    {
-       "errorCode" : {
-         "value" : 0
-       },
-       "result" : {
-         "type"  : "%s",
-         "value" : %s
-      }
-    }
-  )";
-
-  auto doc = json::parse(
-      fmt::sprintf(retJSONTemplate, GetTypeStringForIgorType(fip.returnType),
-                   ExtractReturnStringFromUnion(&retStorage, fip.returnType)));
+  json doc;
+  doc["errorCode"] = {"value", 0};
+  doc["result"]    = {
+      {"type", GetTypeStringForIgorType(fip.returnType)},
+      {"value", ExtractReturnValueFromUnion(&retStorage, fip.returnType)}};
 
   // only serialize the pass-by-ref params if we have some
   if(p.HasPassByRefParameters())

src/SerializeWave.cpp
@@ -318,6 +318,7 @@ std::string WaveToString(int waveType, waveHndl waveHandle)
     return WaveToStringImpl(waveType, waveHandle, 0);
   }
 
+  // WaveToStringImpl returns a JSON quoted string already
   auto resultTemplate = R"({
       "real"     : %s,
       "imag"     : %s
@@ -535,11 +536,11 @@ void AddWaveNoteIfSet(json &doc, waveHndl waveHandle)
 
 } // anonymous namespace
 
-std::string SerializeWave(waveHndl waveHandle)
+json SerializeWave(waveHndl waveHandle)
 {
   if(waveHandle == nullptr)
   {
-    return "null";
+    return nullptr;
   }
 
   const auto waveType       = WaveType(waveHandle);
@@ -567,5 +568,5 @@ std::string SerializeWave(waveHndl waveHandle)
   AddDimensionLabelsFullIfSet(doc, waveHandle, dimSizes);
   AddWaveNoteIfSet(doc, waveHandle);
 
-  return doc.dump(4);
+  return doc;
 }

src/SerializeWave.h
@@ -3,4 +3,4 @@
 // This file is part of the `ZeroMQ-XOP` project and licensed under
 // BSD-3-Clause.
 
-std::string SerializeWave(waveHndl waveHandle);
+json SerializeWave(waveHndl waveHandle);

src/zeromq_test_serializeWave.cpp
@@ -14,7 +14,7 @@ extern "C" int zeromq_test_serializeWave(zeromq_test_serializeWaveParams *p)
 
   DebugOutput(fmt::sprintf("%s\r", __func__));
 
-  auto str = SerializeWave(p->wv);
+  const auto str = SerializeWave(p->wv).dump(4);
 
   DebugOutput(fmt::sprintf("%s: output=%.255s\r", __func__, str));
 

commit ffac2b080ee717b96e127cade0a91acefb041af4

CallFunctionOperation::Call: Construct errorCode as object and not as list
Broken since 11b20a76 (Properly construct JSON object in success case as
well, 2018-01-30).

src/CallFunctionOperation.cpp
@@ -279,7 +279,7 @@ json CallFunctionOperation::Call() const
   }
 
   json doc;
-  doc["errorCode"] = {"value", 0};
+  doc["errorCode"] = {{"value", 0}};
   doc["result"]    = {
       {"type", GetTypeStringForIgorType(fip.returnType)},
       {"value", ExtractReturnValueFromUnion(&retStorage, fip.returnType)}};