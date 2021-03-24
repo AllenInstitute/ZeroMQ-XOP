AllenInstitute/ZeroMQ-XOP

commit 4c54f2e0b77fb7145e1536dddbee6df7344620e0

src: Move from fmt::sprintf to fmt::format
In that way we can leverage the advantages of fmt as we can now just use
{} for outputting something.

In addition we switch from output operator overloading to
fmt::formattter<T>.

We also don't need a custom implementation for outputting std::vector as
that is included in fmt/ranges.h.

src/HelperFunctions.cpp
@@ -132,14 +132,14 @@ void ApplyFlags(double flags)
   {
     throw IgorException(
         UNKNOWN_SET_FLAG,
-        fmt::sprintf("zeromq_set: The flag value %g must positive.\r", flags));
+        fmt::format("zeromq_set: The flag value {} must positive.\r", flags));
   }
 
-  DebugOutput(fmt::sprintf("%s: ZMQ Library Version %d.%d.%d\r", __func__,
-                           ZMQ_VERSION_MAJOR, ZMQ_VERSION_MINOR,
-                           ZMQ_VERSION_PATCH));
+  DebugOutput(fmt::format("{}: ZMQ Library Version {}.{}.{}\r", __func__,
+                          ZMQ_VERSION_MAJOR, ZMQ_VERSION_MINOR,
+                          ZMQ_VERSION_PATCH));
 
-  DebugOutput(fmt::sprintf("%s: git revision %s\r", __func__, GIT_REVISION));
+  DebugOutput(fmt::format("{}: git revision {}\r", __func__, GIT_REVISION));
 
   if((val & ZeroMQ_SET_FLAGS::DEFAULT) == ZeroMQ_SET_FLAGS::DEFAULT)
   {
@@ -172,7 +172,7 @@ void ApplyFlags(double flags)
   {
     throw IgorException(
         UNKNOWN_SET_FLAG,
-        fmt::sprintf("zeromq_set: The flag %g is unknown.\r", flags));
+        fmt::format("zeromq_set: The flag {} is unknown.\r", flags));
   }
 }
 
@@ -183,7 +183,7 @@ std::string GetLastEndPoint(void *s)
   int rc         = zmq_getsockopt(s, ZMQ_LAST_ENDPOINT, buf, &bufSize);
   ZEROMQ_ASSERT(rc == 0);
 
-  DebugOutput(fmt::sprintf("%s: lastEndPoint=%s\r", __func__, buf));
+  DebugOutput(fmt::format("{}: lastEndPoint={}\r", __func__, buf));
   return std::string(buf);
 }
 
@@ -191,7 +191,7 @@ void ToggleIPV6Support(bool enable)
 {
   GET_CLIENT_SOCKET(clientSocket);
 
-  DebugOutput(fmt::sprintf("%s: enable=%d\r", __func__, enable));
+  DebugOutput(fmt::format("{}: enable={}\r", __func__, enable));
 
   const int val = enable;
   auto rc = zmq_setsockopt(clientSocket.get(), ZMQ_IPV6, &val, sizeof(val));
@@ -222,8 +222,8 @@ std::string CallIgorFunctionFromMessage(std::string msg)
       req.CanBeProcessed();
       auto reply = req.Call();
 
-      DebugOutput(fmt::sprintf("%s: Function return value is %.255s\r",
-                               __func__, reply.dump(4)));
+      DebugOutput(fmt::format("{}: Function return value is {:255s}\r",
+                              __func__, reply.dump(4)));
 
       return reply.dump(4);
     }
@@ -245,8 +245,8 @@ int ZeroMQClientSend(std::string payload)
   int rc;
   const auto payloadLength = payload.length();
 
-  DebugOutput(fmt::sprintf("%s: payloadLength=%d, socket=%p\r", __func__,
-                           payloadLength, socket.get()));
+  DebugOutput(fmt::format("{}: payloadLength={}, socket={}\r", __func__,
+                          payloadLength, socket.get()));
 
   // empty
   rc = zmq_send(socket.get(), nullptr, 0, ZMQ_SNDMORE);
@@ -256,7 +256,7 @@ int ZeroMQClientSend(std::string payload)
   rc = zmq_send(socket.get(), payload.c_str(), payloadLength, 0);
   ZEROMQ_ASSERT(rc > 0);
 
-  DebugOutput(fmt::sprintf("%s: rc=%d\r", __func__, rc));
+  DebugOutput(fmt::format("{}: rc={}\r", __func__, rc));
 
   return rc;
 }
@@ -267,8 +267,8 @@ int ZeroMQServerSend(std::string identity, std::string payload)
   int rc;
   const auto payloadLength = payload.length();
 
-  DebugOutput(fmt::sprintf("%s: payloadLength=%d, socket=%p\r", __func__,
-                           payloadLength, socket.get()));
+  DebugOutput(fmt::format("{}: payloadLength={}, socket={}\r", __func__,
+                          payloadLength, socket.get()));
 
   // identity
   rc = zmq_send(socket.get(), identity.c_str(), identity.length(), ZMQ_SNDMORE);
@@ -282,7 +282,7 @@ int ZeroMQServerSend(std::string identity, std::string payload)
   rc = zmq_send(socket.get(), payload.c_str(), payloadLength, 0);
   ZEROMQ_ASSERT(rc > 0);
 
-  DebugOutput(fmt::sprintf("%s: rc=%d\r", __func__, rc));
+  DebugOutput(fmt::format("{}: rc={}\r", __func__, rc));
 
   return rc;
 }

src/SerializeWave.cpp
@@ -104,7 +104,7 @@ T *GetWaveDataPtr(waveHndl waveH)
   if(ret != 0)
   {
     throw std::runtime_error(
-        fmt::sprintf("MDAccessNumericWaveData returned error %d", ret));
+        fmt::format("MDAccessNumericWaveData returned error {}", ret));
   }
 
   return reinterpret_cast<T *>(reinterpret_cast<char *>(*waveH) + dataOffset);
@@ -314,13 +314,13 @@ std::string WaveToString(int waveType, waveHndl waveHandle)
   }
 
   // WaveToStringImpl returns a JSON quoted string already
-  auto resultTemplate = R"({
-      "real"     : %s,
-      "imag"     : %s
-      }
+  auto resultTemplate = R"({{
+      "real"     : {},
+      "imag"     : {}
+      }}
    )";
 
-  return fmt::sprintf(
+  return fmt::format(
       resultTemplate, WaveToStringImpl(waveType, waveHandle, 0),
       WaveToStringImpl(waveType, waveHandle, WavePoints(waveHandle)));
 }
@@ -546,8 +546,8 @@ json SerializeWave(waveHndl waveHandle)
   const auto dimSizes       = GetDimensionSizes(waveHandle);
   const auto dimSizesString = DimensionSizesToString(dimSizes);
 
-  DebugOutput(fmt::sprintf(
-      "%s: waveType=%d, modDate=%d, type=%s, dimSizes=%s, rawData=%.255s\r",
+  DebugOutput(fmt::format(
+      "{}: waveType={}, modDate={}, type={}, dimSizes={}, rawData={:255s}\r",
       __func__, waveType, modDate, type, dimSizesString, rawData));
 
   json doc;

src/zeromq_server_recv.cpp
@@ -49,8 +49,8 @@ extern "C" int zeromq_server_recv(zeromq_server_recvParams *p)
     auto identity = CreateStringFromZMsg(&identityMsg);
     auto msg      = CreateStringFromZMsg(&payloadMsg);
 
-    DebugOutput(fmt::sprintf("%s: numBytes=%d, identity=%s, msg=%.255s\r",
-                             __func__, numBytes, identity, msg));
+    DebugOutput(fmt::format("{}: numBytes={}, identity={}, msg={:255s}\r",
+                            __func__, numBytes, identity, msg));
     break;
   }
 

src/zeromq_test_callfunction.cpp
@@ -13,14 +13,14 @@ extern "C" int zeromq_test_callfunction(zeromq_test_callfunctionParams *p)
   auto msg = GetStringFromHandle(p->msg);
   WMDisposeHandle(p->msg);
 
-  DebugOutput(fmt::sprintf("%s: input=%s\r", __func__, msg));
+  DebugOutput(fmt::format("{}: input={}\r", __func__, msg));
 
   auto retMessage = CallIgorFunctionFromMessage(msg);
 
   auto len = retMessage.size();
 
-  DebugOutput(fmt::sprintf("%s: len=%d, retMessage=%.255s\r", __func__, len,
-                           retMessage));
+  DebugOutput(fmt::format("{}: len={}, retMessage={:255s}\r", __func__, len,
+                          retMessage));
 
   p->result = WMNewHandle(len);
   ASSERT(p->result != nullptr);

src/zeromq_test_serializeWave.cpp
@@ -12,11 +12,11 @@ extern "C" int zeromq_test_serializeWave(zeromq_test_serializeWaveParams *p)
 {
   BEGIN_OUTER_CATCH
 
-  DebugOutput(fmt::sprintf("%s\r", __func__));
+  DebugOutput(fmt::format("{}\r", __func__));
 
   const auto str = SerializeWave(p->wv).dump(4);
 
-  DebugOutput(fmt::sprintf("%s: output=%.255s\r", __func__, str));
+  DebugOutput(fmt::format("{}: output={:255s}\r", __func__, str));
 
   auto len  = str.size();
   p->result = WMNewHandle(len);

src/CallFunctionOperation.cpp
@@ -65,49 +65,9 @@ json ExtractReturnValueFromUnion(IgorTypeUnion *ret, int returnType)
 
 } // anonymous namespace
 
-#ifdef MACIGOR
-
-namespace std
-{
-
-#endif
-
-std::ostream &operator<<(std::ostream &out, std::vector<std::string> vec)
-{
-  fmt::fprintf(out, "%s", "[");
-
-  for(auto it = vec.cbegin(); it != vec.cend(); it++)
-  {
-    if(std::distance(it, vec.cend()) > 1)
-    {
-      fmt::fprintf(out, "%s, ", *it);
-    }
-    else
-    {
-      fmt::fprintf(out, "%s", *it);
-    }
-  }
-
-  fmt::fprintf(out, "%s", "]");
-
-  return out;
-}
-
-#ifdef MACIGOR
-
-} // namespace std
-
-#endif
-
-std::ostream &operator<<(std::ostream &out, CallFunctionOperation op)
-{
-  fmt::fprintf(out, "name=%s, params=%s", op.m_name, op.m_params);
-  return out;
-}
-
 CallFunctionOperation::CallFunctionOperation(json j)
 {
-  DebugOutput(fmt::sprintf("%s: size=%d\r", __func__, j.size()));
+  DebugOutput(fmt::format("{}: size={}\r", __func__, j.size()));
 
   if(j.size() != 1 && j.size() != 2)
   {
@@ -168,12 +128,12 @@ CallFunctionOperation::CallFunctionOperation(json j)
   }
 
   DebugOutput(
-      fmt::sprintf("%s: CallFunction object could be created.\r", __func__));
+      fmt::format("{}: CallFunction object could be created.\r", __func__));
 }
 
 void CallFunctionOperation::CanBeProcessed() const
 {
-  DebugOutput(fmt::sprintf("%s: Data=%s.\r", __func__, *this));
+  DebugOutput(fmt::format("{}: Data={}.\r", __func__, *this));
 
   FunctionInfo fip;
   auto rc = GetFunctionInfo(m_name.c_str(), &fip);
@@ -237,12 +197,12 @@ void CallFunctionOperation::CanBeProcessed() const
     throw RequestInterfaceException(REQ_UNSUPPORTED_FUNC_RET);
   }
 
-  DebugOutput(fmt::sprintf("%s: Request Object can be processed.\r", __func__));
+  DebugOutput(fmt::format("{}: Request Object can be processed.\r", __func__));
 }
 
 json CallFunctionOperation::Call() const
 {
-  DebugOutput(fmt::sprintf("%s: Data=%s.\r", __func__, *this));
+  DebugOutput(fmt::format("{}: Data={}.\r", __func__, *this));
 
   FunctionInfo fip;
   auto rc = GetFunctionInfo(m_name.c_str(), &fip);
@@ -260,8 +220,8 @@ json CallFunctionOperation::Call() const
 
   auto functionAborted = SpinProcess();
 
-  DebugOutput(fmt::sprintf("%s: Call finished with functionAborted=%d\r",
-                           __func__, functionAborted));
+  DebugOutput(fmt::format("{}: Call finished with functionAborted={}\r",
+                          __func__, functionAborted));
 
   if(functionAborted)
   {

src/CallFunctionOperation.h
@@ -12,12 +12,20 @@ public:
   void CanBeProcessed() const;
   json Call() const;
 
-private:
-  friend std::ostream &operator<<(std::ostream &out, CallFunctionOperation op);
+  friend struct fmt::formatter<CallFunctionOperation>;
 
+private:
   std::string m_name;
   std::vector<std::string> m_params;
 };
 
-std::ostream &operator<<(std::ostream &out, CallFunctionOperation op);
-std::ostream &operator<<(std::ostream &out, std::vector<std::string> vec);
+template <>
+struct fmt::formatter<CallFunctionOperation> : fmt::formatter<std::string>
+{
+  // parse is inherited from formatter<std::string>.
+  template <typename FormatContext>
+  auto format(CallFunctionOperation op, FormatContext &ctx)
+  {
+    return format_to(ctx.out(), "name={}, params={}", op.m_name, op.m_params);
+  }
+};

src/CustomExceptions.h
@@ -12,8 +12,8 @@
   {                                                                            \
     throw IgorException(                                                       \
         INTERNAL_ERROR,                                                        \
-        fmt::sprintf("The assertion in %s line %d file %s failed\r", __func__, \
-                     __LINE__, __FILE__));                                     \
+        fmt::format("The assertion in {} line {} file {} failed\r", __func__,  \
+                    __LINE__, __FILE__));                                      \
   }
 
 #define ZEROMQ_ASSERT(A)                                                       \
@@ -22,9 +22,9 @@
     auto err = zmq_errno();                                                    \
     throw IgorException(                                                       \
         INVALID_ARG,                                                           \
-        fmt::sprintf("The zmq library call in %s line %d file "                \
-                     "%s failed with errno=%d and msg=\"%s\"\r",               \
-                     __func__, __LINE__, __FILE__, err, zmq_strerror(err)));   \
+        fmt::format("The zmq library call in {} line {} file "                 \
+                    "{} failed with errno={} and msg=\"{}\"\r",                \
+                    __func__, __LINE__, __FILE__, err, zmq_strerror(err)));    \
   }
 
 class IgorException : public std::exception

commit b10764742d32b0935f89e5cc32fb2730a30ef777

src: Avoid very long debug output strings
Bug introduced in 4c54f2e0 (src: Move from fmt::sprintf to fmt::format, 2021-03-23).

src/HelperFunctions.cpp
@@ -237,7 +237,7 @@ std::string CallIgorFunctionFromMessage(std::string msg)
       req.CanBeProcessed();
       auto reply = req.Call();
 
-      DebugOutput(fmt::format("{}: Function return value is {:255s}\r",
+      DebugOutput(fmt::format("{}: Function return value is {:.255s}\r",
                               __func__, reply.dump(4)));
 
       return reply.dump(4);

src/SerializeWave.cpp
@@ -575,7 +575,7 @@ json SerializeWave(waveHndl waveHandle)
   const auto dimSizesString = DimensionSizesToString(dimSizes);
 
   DebugOutput(fmt::format(
-      "{}: waveType={}, modDate={}, type={}, dimSizes={}, rawData={:255s}\r",
+      "{}: waveType={}, modDate={}, type={}, dimSizes={}, rawData={:.255s}\r",
       __func__, waveType, modDate, type, dimSizesString, rawData));
 
   json doc;

src/zeromq_server_recv.cpp
@@ -49,7 +49,7 @@ extern "C" int zeromq_server_recv(zeromq_server_recvParams *p)
     auto identity = CreateStringFromZMsg(&identityMsg);
     auto msg      = CreateStringFromZMsg(&payloadMsg);
 
-    DebugOutput(fmt::format("{}: numBytes={}, identity={}, msg={:255s}\r",
+    DebugOutput(fmt::format("{}: numBytes={}, identity={}, msg={:.255s}\r",
                             __func__, numBytes, identity, msg));
     break;
   }

src/zeromq_test_callfunction.cpp
@@ -19,7 +19,7 @@ extern "C" int zeromq_test_callfunction(zeromq_test_callfunctionParams *p)
 
   auto len = retMessage.size();
 
-  DebugOutput(fmt::format("{}: len={}, retMessage={:255s}\r", __func__, len,
+  DebugOutput(fmt::format("{}: len={}, retMessage={:.255s}\r", __func__, len,
                           retMessage));
 
   p->result = WMNewHandle(len);

src/zeromq_test_serializeWave.cpp
@@ -16,7 +16,7 @@ extern "C" int zeromq_test_serializeWave(zeromq_test_serializeWaveParams *p)
 
   const auto str = SerializeWave(p->wv).dump(4);
 
-  DebugOutput(fmt::format("{}: output={:255s}\r", __func__, str));
+  DebugOutput(fmt::format("{}: output={:.255s}\r", __func__, str));
 
   auto len  = str.size();
   p->result = WMNewHandle(len);