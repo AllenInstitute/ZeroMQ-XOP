AllenInstitute/ZeroMQ-XOP

commit 768c8cb9f365545249c723983df97beaed5d79db

Add Publisher/Subscriber functionality
This adds the following new functions:

zeromq_pub_bind
zeromq_pub_send

zeromq_sub_connect
zeromq_sub_recv
zeromq_sub_add_filter
zeromq_sub_remove_filter

and allows now to use ZeroMQ PUB/SUB [1] sockets.

It also defines a new internal class HeartbeatPublisher which will send
out a empty message every 5 seconds of filter type ZeroMQ_HEARTBEAT.
This is enabled only when a publisher socket is available.

[1]: https://zguide.zeromq.org/docs/chapter5/

src/GlobalData.cpp
@@ -31,6 +31,9 @@ void ApplySocketDefaults(void *s, SocketTypes st)
     ZEROMQ_ASSERT(rc == 0);
     return;
   }
+  case SocketTypes::Publisher:
+    // do nothing
+    return;
   case SocketTypes::Server:
   {
     int valOne = 1;
@@ -44,6 +47,9 @@ void ApplySocketDefaults(void *s, SocketTypes st)
     ZEROMQ_ASSERT(rc == 0);
     return;
   }
+  case SocketTypes::Subscriber:
+    // do nothing
+    return;
   }
 
   ASSERT(0);
@@ -55,8 +61,12 @@ int GetZeroMQSocketConstant(SocketTypes st)
   {
   case SocketTypes::Client:
     return ZMQ_DEALER;
+  case SocketTypes::Publisher:
+    return ZMQ_PUB;
   case SocketTypes::Server:
     return ZMQ_ROUTER;
+  case SocketTypes::Subscriber:
+    return ZMQ_SUB;
   }
 
   ASSERT(0);
@@ -65,7 +75,8 @@ int GetZeroMQSocketConstant(SocketTypes st)
 
 AllSocketTypesArray GetAllSocketTypes()
 {
-  return AllSocketTypesArray{SocketTypes::Client, SocketTypes::Server};
+  return AllSocketTypesArray{SocketTypes::Client, SocketTypes::Subscriber,
+                             SocketTypes::Publisher, SocketTypes::Server};
 }
 
 GlobalData::GlobalData()
@@ -142,6 +153,10 @@ bool GlobalData::GetRecvBusyWaitingFlag() const
 
 void GlobalData::CloseConnections()
 {
+  if(HasSocket(SocketTypes::Subscriber))
+  {
+    RemoveSubscriberMessageFilter("");
+  }
 
   for(auto st : GetAllSocketTypes())
   {
@@ -167,11 +182,13 @@ void GlobalData::CloseConnections()
         switch(st)
         {
         case SocketTypes::Server:
+        case SocketTypes::Publisher:
           rc = zmq_disconnect(socket.get(), conn.c_str());
           DEBUG_OUTPUT("zmq_disconnect({}) returned={}", conn, rc);
 
           break;
         case SocketTypes::Client:
+        case SocketTypes::Subscriber:
           rc = zmq_unbind(socket.get(), conn.c_str());
           DEBUG_OUTPUT("zmq_unbind({}) returned={}", conn, rc);
           break;
@@ -257,14 +274,78 @@ void GlobalData::InitLogging()
   m_loggingSink = std::make_unique<Logging>(PACKAGE_NAME);
 }
 
+void GlobalData::AddSubscriberMessageFilter(std::string filter)
+{
+  DEBUG_OUTPUT("filter={}", filter);
+
+  const auto st = SocketTypes::Subscriber;
+
+  GET_SOCKET(socket, st);
+
+  auto it = std::find(std::begin(m_subMessageFilters),
+                      std::end(m_subMessageFilters), filter);
+
+  if(it != std::end(m_subMessageFilters))
+  {
+    throw IgorException(MESSAGE_FILTER_DUPLICATED);
+  }
+
+  auto rc = zmq_setsockopt(socket.get(), ZMQ_SUBSCRIBE, filter.c_str(),
+                           filter.size());
+  ZEROMQ_ASSERT(rc == 0);
+
+  m_subMessageFilters.emplace_back(std::move(filter));
+}
+
+void GlobalData::RemoveSubscriberMessageFilter(const std::string &filter)
+{
+  DEBUG_OUTPUT("filter={}", filter);
+
+  const auto st = SocketTypes::Subscriber;
+
+  GET_SOCKET(socket, st);
+
+  auto unsubscribe = [&socket](const std::string &entry) -> void {
+    auto rc = zmq_setsockopt(socket.get(), ZMQ_UNSUBSCRIBE, entry.c_str(),
+                             entry.size());
+    ZEROMQ_ASSERT(rc == 0);
+  };
+
+  if(filter.empty())
+  {
+    for(auto &entry : m_subMessageFilters)
+    {
+      unsubscribe(entry);
+    }
+    m_subMessageFilters.clear();
+  }
+  else
+  {
+    auto it = std::find(std::begin(m_subMessageFilters),
+                        std::end(m_subMessageFilters), filter);
+
+    if(it == std::end(m_subMessageFilters))
+    {
+      throw IgorException(MESSAGE_FILTER_MISSING);
+    }
+
+    unsubscribe(filter);
+    m_subMessageFilters.erase(it);
+  }
+}
+
 std::recursive_mutex &GlobalData::GetMutex(SocketTypes st)
 {
   switch(st)
   {
   case SocketTypes::Client:
     return m_client.m_mutex;
+  case SocketTypes::Publisher:
+    return m_pub.m_mutex;
   case SocketTypes::Server:
     return m_server.m_mutex;
+  case SocketTypes::Subscriber:
+    return m_sub.m_mutex;
   }
 
   ASSERT(0);
@@ -276,8 +357,12 @@ GlobalData::SocketTypeData &GlobalData::GetSocketTypeData(SocketTypes st)
   {
   case SocketTypes::Client:
     return m_client;
+  case SocketTypes::Publisher:
+    return m_pub;
   case SocketTypes::Server:
     return m_server;
+  case SocketTypes::Subscriber:
+    return m_sub;
   }
 
   ASSERT(0);

src/HelperFunctions.cpp
@@ -353,6 +353,26 @@ int ZeroMQServerSend(const std::string &identity, const std::string &payload)
   return rc;
 }
 
+int ZeroMQPublisherSend(const std::string &filter, const std::string &payload)
+{
+  GET_SOCKET(socket, SocketTypes::Publisher);
+
+  DEBUG_OUTPUT("filterLength={}, payloadLength={}, socket={}", filter.length(),
+               payload.length(), socket.get());
+
+  // filter
+  int rc = zmq_send(socket.get(), filter.c_str(), filter.length(), ZMQ_SNDMORE);
+  ZEROMQ_ASSERT(rc >= 0);
+
+  // payload
+  rc = zmq_send(socket.get(), payload.c_str(), payload.length(), 0);
+  ZEROMQ_ASSERT(rc >= 0);
+
+  DEBUG_OUTPUT("rc={}", rc);
+
+  return rc;
+}
+
 /// Expect three frames:
 /// - identity
 /// - empty
@@ -405,6 +425,34 @@ int ZeroMQClientReceive(zmq_msg_t *payloadMsg)
   // zeromq guarantees that either all parts in multi-part messages
   // arrive or none.
   if(!zmq_msg_more(payloadMsg))
+  {
+    throw IgorException(INVALID_MESSAGE_FORMAT);
+  }
+
+  numBytes = zmq_msg_recv(payloadMsg, socket.get(), 0);
+
+  if(numBytes < 0 || zmq_msg_more(payloadMsg))
+  {
+    throw IgorException(INVALID_MESSAGE_FORMAT);
+  }
+
+  return numBytes;
+}
+
+/// Expect two frames:
+/// - filter
+/// - payload
+int ZeroMQSubscriberReceive(zmq_msg_t *filterMsg, zmq_msg_t *payloadMsg)
+{
+  GET_SOCKET(socket, SocketTypes::Subscriber);
+  auto numBytes = zmq_msg_recv(filterMsg, socket.get(), 0);
+
+  if(numBytes < 0)
+  {
+    return numBytes;
+  }
+
+  if(!zmq_msg_more(filterMsg))
   {
     throw IgorException(INVALID_MESSAGE_FORMAT);
   }
@@ -738,8 +786,10 @@ void DoBindOrConnect(Handle &h, SocketTypes st)
   switch(st)
   {
   case SocketTypes::Client:
+  case SocketTypes::Publisher:
     rc = zmq_connect(socket.get(), point.c_str());
     break;
+  case SocketTypes::Subscriber:
   case SocketTypes::Server:
     rc = zmq_bind(socket.get(), point.c_str());
     break;

procedures/ZeroMQ_Interop.ipf
@@ -21,6 +21,8 @@ Constant ZeroMQ_SET_FLAGS_LOGGING = 0x10
 
 /// @}
 
+StrConstant ZeroMQ_HEARTBEAT = "heartbeat"
+
 /// @name Error codes
 /// @anchor ZeroMQErrorCodes
 /// @{

src/CMakeLists.txt
@@ -66,6 +66,7 @@ ENDIF()
 SET(COVERAGE_SOURCES
   CallFunctionOperation.cpp
   CallFunctionParameterHandler.cpp
+  HeartbeatPublisher.cpp
   HistoryGrabber.cpp
   Logging.cpp
   MessageHandler.cpp
@@ -80,6 +81,12 @@ SET(COVERAGE_SOURCES
   zeromq_server_bind.cpp
   zeromq_server_recv.cpp
   zeromq_server_send.cpp
+  zeromq_pub_bind.cpp
+  zeromq_pub_send.cpp
+  zeromq_sub_connect.cpp
+  zeromq_sub_add_filter.cpp
+  zeromq_sub_recv.cpp
+  zeromq_sub_remove_filter.cpp
   zeromq_set.cpp
   zeromq_handler_start.cpp
   zeromq_stop.cpp
@@ -116,6 +123,7 @@ SET(HEADERS
   Errors.h
   functions.h
   GlobalData.h
+  HeartbeatPublisher.h
   HelperFunctions.h
   HistoryGrabber.h
   IgorTypeUnion.h

src/Errors.h
@@ -15,6 +15,8 @@
 #define MISSING_PROCEDURE_FILES    8 + FIRST_XOP_ERR
 #define INVALID_MESSAGE_FORMAT     9 + FIRST_XOP_ERR
 #define INVALID_LOGGING_TEMPLATE   10 + FIRST_XOP_ERR
+#define MESSAGE_FILTER_DUPLICATED  11 + FIRST_XOP_ERR
+#define MESSAGE_FILTER_MISSING     12 + FIRST_XOP_ERR
 
 // non-XOP error codes
 

src/GlobalData.h
@@ -12,10 +12,12 @@
 enum class SocketTypes
 {
   Client,
-  Server
+  Server,
+  Publisher,
+  Subscriber
 };
 
-using AllSocketTypesArray = std::array<SocketTypes, 2>;
+using AllSocketTypesArray = std::array<SocketTypes, 4>;
 
 AllSocketTypesArray GetAllSocketTypes();
 
@@ -53,6 +55,10 @@ public:
   void SetLoggingTemplate(const std::string &loggingTemplate);
   void InitLogging();
 
+  void AddSubscriberMessageFilter(std::string filter);
+
+  void RemoveSubscriberMessageFilter(const std::string &filter);
+
 private:
   GlobalData();
   ~GlobalData()                  = default;
@@ -62,7 +68,7 @@ private:
   struct SocketTypeData
   {
     std::vector<std::string> m_list; // list of connections or binds
-    void *m_zmq_socket{};
+    void *m_zmq_socket{nullptr};
     std::recursive_mutex m_mutex;
   };
 
@@ -81,6 +87,7 @@ private:
   std::unique_ptr<Logging> m_loggingSink;
   std::recursive_mutex m_loggingLock;
   void *zmq_context;
+  std::vector<std::string> m_subMessageFilters;
 };
 
 template <>
@@ -98,6 +105,12 @@ struct fmt::formatter<SocketTypes> : fmt::formatter<std::string>
       break;
     case SocketTypes::Server:
       name = "Server";
+      break;
+    case SocketTypes::Publisher:
+      name = "Publisher";
+      break;
+    case SocketTypes::Subscriber:
+      name = "Subscriber";
       break;
     }
 

src/HeartbeatPublisher.cpp
@@ -1,0 +1,116 @@
+#include "ZeroMQ.h"
+#include "HeartbeatPublisher.h"
+#include "RequestInterface.h"
+
+#include <chrono>
+#include <thread>
+
+// This file is part of the `ZeroMQ-XOP` project and licensed under
+// BSD-3-Clause.
+
+namespace
+{
+
+using namespace std::chrono_literals;
+std::recursive_mutex threadMutex;
+bool threadShouldFinish;
+std::recursive_mutex threadShouldFinishMutex;
+
+void WorkerThread()
+{
+  DEBUG_OUTPUT("Begin");
+
+  {
+    // initialize to false
+    LockGuard lock(threadShouldFinishMutex);
+    threadShouldFinish = false;
+  }
+
+  for(;;)
+  {
+    try
+    {
+      // check if stop is requested
+      {
+        LockGuard lock(threadShouldFinishMutex);
+        if(threadShouldFinish)
+        {
+          DEBUG_OUTPUT("Exiting");
+          break;
+        }
+      }
+
+      if(GlobalData::Instance().HasBindsOrConnections(SocketTypes::Publisher))
+      {
+        auto rc = ZeroMQPublisherSend("heartbeat", "");
+
+        if(rc)
+        {
+          EMERGENCY_OUTPUT(
+              "Error sending heartbeat publisher message with rc = {}", rc);
+          continue;
+        }
+      }
+
+      std::this_thread::sleep_for(5s);
+    }
+    catch(const IgorException &e)
+    {
+      EMERGENCY_OUTPUT(
+          "Caught IgorException with what = \"{}\". This must NOT happen!",
+          e.what());
+    }
+    catch(const std::exception &e)
+    {
+      EMERGENCY_OUTPUT(
+          "Caught std::exception with what = \"{}\". This must NOT happen!",
+          e.what());
+    }
+    catch(...)
+    {
+      EMERGENCY_OUTPUT("Caught exception. This must NOT happen!");
+    }
+  }
+}
+
+} // anonymous namespace
+
+void HeartbeatPublisher::Start()
+{
+  LockGuard lock(threadMutex);
+
+  if(m_thread.joinable())
+  {
+    throw IgorException(INTERNAL_ERROR,
+                        "Can not start heartbeat publisher twice");
+  }
+
+  DEBUG_OUTPUT("Trying to start.");
+
+  auto t = std::thread(WorkerThread);
+  m_thread.swap(t);
+}
+
+void HeartbeatPublisher::Stop()
+{
+  LockGuard lock(threadMutex);
+
+  if(!m_thread.joinable())
+  {
+    return;
+  }
+
+  DEBUG_OUTPUT("Shutting down.");
+
+  {
+    LockGuard innerLock(threadShouldFinishMutex);
+    threadShouldFinish = true;
+  }
+
+  m_thread.join();
+}
+
+HeartbeatPublisher::~HeartbeatPublisher()
+{
+  Stop();
+}

src/HeartbeatPublisher.h
@@ -1,0 +1,29 @@
+#pragma once
+
+#include "ZeroMQ.h"
+
+// This file is part of the `ZeroMQ-XOP` project and licensed under
+// BSD-3-Clause.
+
+class HeartbeatPublisher
+{
+public:
+  /// Access to singleton-type global object
+  static HeartbeatPublisher &Instance()
+  {
+    static HeartbeatPublisher obj;
+    return obj;
+  }
+
+  void Start();
+  void Stop();
+
+private:
+  HeartbeatPublisher() = default;
+  ~HeartbeatPublisher();
+  HeartbeatPublisher(const HeartbeatPublisher &) = delete;
+  HeartbeatPublisher &operator=(const HeartbeatPublisher &) = delete;
+
+  class thread;
+  std::thread m_thread;
+};

commit 70fbd2918dd99bcc72ddca4b73a41074fccd4424

src: Use correct bind/connect methods for Pub/Sub
We need to use zmq_connect/zmq_disconnect for Subscriber and
zmq_bind/zmq_unbind for Publisher.

This was not caught in our tests because somehow this works when done in
the same process nevertheless.

Bug introduced in 768c8cb9 (Add Publisher/Subscriber functionality, 2021-04-05).

This also finally explains the asymmetry between server/client sockets
vs publisher/subscriber sockets as mentioned in f2d4eff9 (Tests: Extend
existing tests to also cover pub/sub, 2021-04-06). There it was observed
that the publisher socket did not bind to a port as reported by netstat.

src/GlobalData.cpp
@@ -183,13 +183,13 @@ void GlobalData::CloseConnections()
         {
         case SocketTypes::Server:
         case SocketTypes::Publisher:
-          rc = zmq_disconnect(socket.get(), conn.c_str());
+          rc = zmq_unbind(socket.get(), conn.c_str());
           DEBUG_OUTPUT("zmq_disconnect({}) returned={}", conn, rc);
 
           break;
         case SocketTypes::Client:
         case SocketTypes::Subscriber:
-          rc = zmq_unbind(socket.get(), conn.c_str());
+          rc = zmq_disconnect(socket.get(), conn.c_str());
           DEBUG_OUTPUT("zmq_unbind({}) returned={}", conn, rc);
           break;
         }

src/HelperFunctions.cpp
@@ -786,11 +786,11 @@ void DoBindOrConnect(Handle &h, SocketTypes st)
   switch(st)
   {
   case SocketTypes::Client:
-  case SocketTypes::Publisher:
+  case SocketTypes::Subscriber:
     rc = zmq_connect(socket.get(), point.c_str());
     break;
-  case SocketTypes::Subscriber:
   case SocketTypes::Server:
+  case SocketTypes::Publisher:
     rc = zmq_bind(socket.get(), point.c_str());
     break;
   }

tests/zmq_bind.ipf
@@ -73,26 +73,21 @@ Function ComplainsWithInvalidArg2([string str])
 End
 
 // bind does not accept names
-Function ComplainsWithInvalidArg3AndServer()
+// UTF_TD_GENERATOR zmq_bind#BindLikeFunctions
+Function ComplainsWithInvalidArg3([string str])
 
 	variable err, ret
 
+	FUNCREF BIND_PROTOTYPE f = $str
+
 	try
-		ret = zeromq_server_bind("tcp://localhost:5555"); AbortOnRTE
+		ret = f("tcp://localhost:5555"); AbortOnRTE
 		FAIL()
 	catch
 		err = GetRTError(1)
 		CheckErrorMessage(err, ZeroMQ_INVALID_ARG)
 	endtry
 
-	CHECK_EQUAL_VAR(ret, 0)
-End
-
-Function WorksWithNamesAndPublisher()
-
-	variable ret
-
-	ret = zeromq_pub_bind("tcp://localhost:5555")
 	CHECK_EQUAL_VAR(ret, 0)
 End
 
@@ -104,8 +99,11 @@ Function BindsToLocalHost([string str])
 	FUNCREF BIND_PROTOTYPE f = $str
 	skipSourceCheck = GetSourceCheck_IGNORE(str)
 
+	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(5555, TCP_V4), 0)
+
 	ret = f("tcp://127.0.0.1:5555")
 	CHECK_EQUAL_VAR(ret, 0)
+	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(5555, TCP_V4), 1)
 End
 
 // UTF_TD_GENERATOR zmq_bind#BindLikeFunctions
@@ -113,12 +111,15 @@ Function BindsToLocalHostIPV6([string str])
 
 	variable ret, skipSourceCheck
 
+	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(5555, TCP_V6), 0)
+
 	FUNCREF BIND_PROTOTYPE f = $str
 	skipSourceCheck = GetSourceCheck_IGNORE(str)
 
 	zeromq_set(ZeroMQ_SET_FLAGS_IPV6)
 	ret = f("tcp://::1:5555")
 	CHECK_EQUAL_VAR(ret, 0)
+	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(5555, TCP_V6), 1)
 End
 
 // UTF_TD_GENERATOR zmq_bind#BindLikeFunctions
@@ -126,44 +127,43 @@ Function BindsToLocalHostIPV6AndIPV4([string str])
 
 	variable ret, skipSourceCheck
 
+	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(5555, TCP_V4), 0)
+	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(6666, TCP_V6), 0)
+
 	FUNCREF BIND_PROTOTYPE f = $str
 	skipSourceCheck = GetSourceCheck_IGNORE(str)
 
 	ret = f("tcp://127.0.0.1:5555")
 	CHECK_EQUAL_VAR(ret, 0)
+	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(5555, TCP_V4), 1)
+
 	// the ipv6 flag juggling is required due to https://github.com/zeromq/libzmq/issues/853
 	zeromq_set(ZeroMQ_SET_FLAGS_IPV6)
 	ret = f("tcp://::1:6666")
 	CHECK_EQUAL_VAR(ret, 0)
+
+	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(6666, TCP_V6), 1)
 End
 
-Function ComplainsOnBindOnUsedPortServer()
+// UTF_TD_GENERATOR zmq_bind#BindLikeFunctions
+Function ComplainsOnBindOnUsedPortServer([string str])
 
 	variable err, ret
 
+	FUNCREF BIND_PROTOTYPE f = $str
+
 	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(5555, TCP_V4), 0)
-	ret = zeromq_server_bind("tcp://127.0.0.1:5555")
+	ret = f("tcp://127.0.0.1:5555")
 	CHECK_EQUAL_VAR(ret, 0)
 	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(5555, TCP_V4), 1)
 
 	try
-		ret = zeromq_server_bind("tcp://127.0.0.1:5555"); AbortOnRTE
+		ret = f("tcp://127.0.0.1:5555"); AbortOnRTE
 		FAIL()
 	catch
 		err = GetRTError(1)
 		CheckErrorMessage(err, ZeroMQ_INVALID_ARG)
 	endtry
-End
-
-Function BindMultipleTimesWorksWithPublisher()
-
-	variable ret
-
-	ret = zeromq_pub_bind("tcp://127.0.0.1:5555")
-	CHECK_EQUAL_VAR(ret, 0)
-
-	ret = zeromq_pub_bind("tcp://127.0.0.1:5555")
-	CHECK_EQUAL_VAR(ret, 0)
 End
 
 // UTF_TD_GENERATOR zmq_bind#BindLikeFunctions
@@ -174,11 +174,15 @@ Function AllowsBindingMultiplePorts([string str])
 	FUNCREF BIND_PROTOTYPE f = $str
 	skipSourceCheck = GetSourceCheck_IGNORE(str)
 
+	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(5555, TCP_V4), 0)
 	ret = f("tcp://127.0.0.1:5555")
 	CHECK_EQUAL_VAR(ret, 0)
+	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(5555, TCP_V4), 1)
 
+	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(6666, TCP_V4), 0)
 	ret = f("tcp://127.0.0.1:6666")
 	CHECK_EQUAL_VAR(ret, 0)
+	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(6666, TCP_V4), 1)
 End
 
 Function DoesNotAcceptLargeMessagesOnServerSocket()

tests/zmq_connect.ipf
@@ -84,34 +84,23 @@ static Function ComplainsWithInvalidArg2([string str])
 	CHECK_EQUAL_VAR(ret, 0)
 End
 
-Function ConnectionsAcceptsHostNameWithClient()
+// UTF_TD_GENERATOR zmq_connect#ConnectLikeFunctions
+Function ConnectionsAcceptsHostName([string str])
 
 	variable ret
 
-	ret = zeromq_client_connect("tcp://localhost:5555")
+	FUNCREF CONNECT_PROTOTYPE f = $str
+
+	ret = f("tcp://localhost:5555")
 	CHECK_EQUAL_VAR(ret, 0)
 End
-
-static Function ComplainsWithHostnameAndSubscriber()
-
-	variable err, ret
-
-	try
-		ret = zeromq_sub_connect("tcp://localhost:5555"); AbortOnRTE
-		FAIL()
-	catch
-		err = GetRTError(1)
-		CheckErrorMessage(err, ZeroMQ_INVALID_ARG)
-	endtry
-
-	CHECK_EQUAL_VAR(ret, 0)
-End
-
 
 // UTF_TD_GENERATOR zmq_connect#ConnectLikeFunctions
 Function ConnectionOrderDoesNotMatter1([string str])
 
 	variable ret, skipSourceCheck
+
+	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(5555, TCP_V4), 0)
 
 	FUNCREF CONNECT_PROTOTYPE f = $str
 	skipSourceCheck = GetSourceCheck_IGNORE(str)
@@ -128,6 +117,8 @@ End
 Function ConnectionOrderDoesNotMatter2([string str])
 
 	variable ret, skipSourceCheck
+
+	CHECK_EQUAL_VAR(GetListeningStatus_IGNORE(5555, TCP_V4), 0)
 
 	FUNCREF CONNECT_PROTOTYPE f = $str
 	skipSourceCheck = GetSourceCheck_IGNORE(str)