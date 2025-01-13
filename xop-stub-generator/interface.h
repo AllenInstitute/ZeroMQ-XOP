a6c758d8fa5eea1ad5417f64608eb8451da0e5c2
Thomas Braun <thomas.braun@byte-physics.de>
2025-01-13 21:33:00 +0100

    examples/xop-stub-generator: Apply clang-format

--- xop-stub-generator/interface.h
+++ xop-stub-generator/interface.h
@@ -1,6 +1,7 @@
 /// @file interface.h Available functions in the ZeroMQ XOP
 ///
-/// This file is part of the `ZeroMQ-XOP` project and licensed under BSD-3-Clause.
+/// This file is part of the `ZeroMQ-XOP` project and licensed under
+/// BSD-3-Clause.
 ///
 /// - All functions returning a variable always return zero
 /// - Errors are signalled via RTE, use
@@ -19,7 +20,8 @@
 /// - For compatibility reasons with the REQ socket, DEALER sockets have to
 ///   always send two frames: An empty one and one with the payload.
 
-/// @brief Stop all listening binds and disconnect outgoing connections, stops also the message handler.
+/// @brief Stop all listening binds and disconnect outgoing connections, stops
+/// also the message handler.
 ///        Internally closes also the sockets.
 ///
 ///        Will be implicitly called on Igor close.
@@ -50,7 +52,8 @@ THREADSAFE variable zeromq_server_send(string identity, string msg);
 ///
 /// Implemented using a blocking wait albeit abortable from within Igor Pro.
 ///
-/// @param[out] identity client identifier, required for sending a message back via zeromq_server_send()
+/// @param[out] identity client identifier, required for sending a message back
+/// via zeromq_server_send()
 ///
 /// @return received message
 THREADSAFE string zeromq_server_recv(string *identity);
@@ -60,9 +63,11 @@ THREADSAFE string zeromq_server_recv(string *identity);
 /// @param msg      Message to send
 THREADSAFE variable zeromq_client_send(string msg);
 
-/// @brief Connect to a ZMQ server, e.g. a different Igor instance running as a server
+/// @brief Connect to a ZMQ server, e.g. a different Igor instance running as a
+/// server
 ///
-/// @param remotePoint Protocol and address of the server, usually something like `tcp://127.0.0.1:5670`
+/// @param remotePoint Protocol and address of the server, usually something
+/// like `tcp://127.0.0.1:5670`
 THREADSAFE variable zeromq_client_connect(string remotePoint);
 
 /// Receive a message as the `DEALER` socket
@@ -86,28 +91,34 @@ THREADSAFE variable zeromq_pub_bind(string localPoint);
 
 /// @brief Publish the given message to all connected subscribers
 ///
-/// Message filtering happens on the publisher side, around ZMQ_SNDHWM messages (by default 1000) will be kept.
+/// Message filtering happens on the publisher side, around ZMQ_SNDHWM messages
+/// (by default 1000) will be kept.
 THREADSAFE variable zeromq_pub_send(string filter, string msg);
 
 /// @brief Connect to a ZMQ_PUB socket as ZMQ_SUB
 ///
-/// @param remotePoint Protocol and address of the server, usually something like `tcp://127.0.0.1:5670`
+/// @param remotePoint Protocol and address of the server, usually something
+/// like `tcp://127.0.0.1:5670`
 ///
-/// By default there are no subscriptions, be sure to call zeromq_sub_add_filter at least once.
+/// By default there are no subscriptions, be sure to call zeromq_sub_add_filter
+/// at least once.
 THREADSAFE variable zeromq_sub_connect(string remotePoint);
 
 /// @brief Add a message filter
 ///
-/// @param filter message type to add a subscription for. Using an empty string will subscribe to all messages.
+/// @param filter message type to add a subscription for. Using an empty string
+/// will subscribe to all messages.
 ///
-/// Multiple filters are or'ed, which means a message must match at least one filter.
+/// Multiple filters are or'ed, which means a message must match at least one
+/// filter.
 ///
 /// The filter types are binary compared.
 THREADSAFE variable zeromq_sub_add_filter(string filter);
 
 /// @brief Removes a previously added message filter
 ///
-/// @param filter message type to remove a subscription for. Use an empty string to unsubscribe from all messages.
+/// @param filter message type to remove a subscription for. Use an empty string
+/// to unsubscribe from all messages.
 THREADSAFE variable zeromq_sub_remove_filter(string filter);
 
 /// @brief Receive subscribed messages
@@ -129,7 +140,8 @@ variable zeromq_handler_stop();
 ///
 /// Set the JSON text used as template for the JSONL log file.
 ///
-/// This function does not enable logging, use `zeromq_set(ZeroMQ_SET_FLAGS_LOGGING)` for that.
+/// This function does not enable logging, use
+/// `zeromq_set(ZeroMQ_SET_FLAGS_LOGGING)` for that.
 ///
 /// Requirement:
 /// - Valid JSON text
